f228af91e2c50838c273341d389662a512fc08c8
Vladimir <vladimirplotnikov371@gmil.om>
2023-09-08 16:07:57 +0300

    edit profile

---
+++ src/components/screens/profile/profile.tsx
@@ -0,0 +1,51 @@
+import { UserUpdate } from "@/store/user/types"
+import { FC } from "react"
+import classes from './profile.module.scss'
+import Button from "@/ui/buttons/button"
+import { useRouter } from "next/navigation"
+
+const Profile: FC<{ profile: UserUpdate }> = ({ profile }) => {
+ const router = useRouter()
+ return (
+  <div className={classes.profile}>
+   <div className={classes.content}>
+    <h6>Name:</h6>
+    {profile.name}
+   </div>
+   <div className={classes.content}>
+    <h6>Email:</h6>
+    {profile.email}
+   </div>
+   <div className={classes.content}>
+    <h6>Phone:</h6>
+    {profile.phone}
+   </div>
+   <div className={classes.content}>
+    <Button
+     onClick={()=>router.push("/edit_profile")}
+     variant="light"
+     size="sm"
+     className="w-24">
+     Edit
+    </Button>
+    <Button
+     onClick={()=>router.push("/my_orders")}
+     variant="light"
+    size="sm"
+    className="w-24"
+    >
+     My Orders
+    </Button>
+    <Button
+     size="sm"
+     className="w-24"
+     onClick={()=>router.push("/delete_profile")}
+     variant="dark">
+     Delete
+    </Button>
+    </div>
+  </div>
+ )
+}
+
+export default Profile
---
+++ src/pages/edit_profile.tsx
@@ -0,0 +1,31 @@
+
+import EditProfileForm from "@/components/screens/profileEditForm/editProfile"
+import { useProfile } from "@/hooks/querries/useProfile"
+import { NextPageAuth } from "@/providers/authProviders/types"
+import { UserUpdate } from "@/store/user/types"
+import Heading from "@/ui/heading/heading"
+import Layout from "@/ui/layout/layout"
+import Meta from "@/ui/meta/meta"
+
+
+const EditProfilePage: NextPageAuth = () => {
+ const { profile } = useProfile()
+ const data: UserUpdate = {
+  email: profile?.email,
+  name: profile?.name,
+  password: "",
+  phone: profile?.phone
+ }
+    return (
+      <Meta title = "Edit Profile Page">
+      <Layout >
+        <EditProfileForm profile={data} />
+        </Layout>
+        </Meta>
+   
+    )
+   }
+   
+
+   EditProfilePage.isOnlyUser = true
+   export default EditProfilePage
